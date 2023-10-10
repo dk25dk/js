413116b1595c920af37a2721556ce5c2111849cb
Dilip <dilip1564dk@gmail.com>
2023-10-11 01:03:21 +0530

    first commit

---
+++ if_elsestatment.js
@@ -0,0 +1,42 @@
+/*
+    let age = 4;
+    if(age > 18){
+        console.log("eligible for vote");
+    }
+    else{
+        console.log("sorry please try after sometime");
+    }*/
+
+
+/*
+    let age = 44;
+    if(age > 40 && age <=60){
+        console.log("your age is between 40 to 60");
+    }
+    else if(age >20 && age <=40){
+        console.log("your age is between 20 to 40");
+    }
+    else if (age >10 && age <=20){
+        console.log("your age is between 10 to 20");
+    }else{
+        console.log("no more");
+    }
+*/
+
+
+  
+    let age = 44;
+    let locationDetails = "bangalore";
+    if(age > 18){
+        console.log("eligible for vote");
+        if(locationDetails == "bangalore"){
+            console.log("bangalore");
+        }
+        else{
+           console.log("Dehli");
+        }
+    }
+    else{
+        console.log("sorry please try after sometime");
+    }
+   
---
+++ script.js
@@ -0,0 +1,9 @@
+const accountId = 123456;
+let accountEmail = "dk1234@gmail.com";
+var accountPassword = "12212"
+
+// accountId = 234556;
+accountEmail = "dkr13@gmail.com";
+accountPassword = "123123";
+
+console.table([accountId , accountEmail, accountPassword]);
